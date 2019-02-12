8762a1949e01c69a2310bea84db3580ad4dcba40
yl5504 <43120402+yl5504@users.noreply.github.com>
2019-02-12 18:53:53 -0500

    Add files via upload

---
+++ yuhan_clip.sh
@@ -0,0 +1,29 @@
+#Yuhan's 40 second -- happiness
+
+ffmpeg -i 1.mp4 -ss 00:27 -to 00:37 -c:v copy -c:a copy a.mp4
+
+ffmpeg -i 2.mp4 -ss 04:40 -to 04:50 -c:v copy -c:a copy b.mp4
+
+ffmpeg -i 3.mp4 -ss 03:20 -to 03:20 -c:v copy -c:a copy c.mp4
+
+ffmpeg -i 4.mp4 -ss 00:15 -to 00:25 -c:v copy -c:a copy 4_output.mp4
+
+ffmpeg -i 4_output.mp4 -vf hflip -c:a copy d.mp4
+
+ffmpeg -start_number 000000060 -i 'defaultCanvas-%9d.png' -c:v libx264 -pix_fmt yuv420p image.mp4
+
+ffmpeg -i image.mp4 -filter:v "setpts=2.0*PTS" e.mp4
+
+$ cat mylist.txt
+file 'a.mp4'
+file 'b.mp4'
+file 'c.mp4'
+file 'd.mp4'
+file 'e.mp4'
+
+ffmpeg -f concat -i mylist.txt -c copy output.mp4
+
+ffmpeg -i output.mp4 -i happiness.mp3 -c copy -map 0:v:0 -map 1:a:0 yuhan_clip.mp4
+
+#transfer to MTS
+ffmpeg -i yuhan_clip.mp4 -q 0 yuhan_clip.MTS
